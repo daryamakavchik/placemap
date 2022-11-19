93c912f7cb6d949ec4a6eb7f55d7e00f5acefe94
daryamakavchik <darjamakavchik@gmail.com>
2022-11-18 16:05:15 -0800

    add react

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { WebMapView } from './WebMap';
+
+
+ReactDOM.render(
+    <WebMapView />,
+    document.getElementById('root'),
+);
