2df74837b65fb9c838b632bae31bf3398c0c7cd1
daryamakavchik <darjamakavchik@gmail.com>
2022-11-18 18:15:40 -0800

    add custom features

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,10 @@
 import React, { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
+import Graphic from "@arcgis/core/Graphic";
+import Search from "@arcgis/core/widgets/Search";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import * as locator from "@arcgis/core/rest/locator";
 import esriConfig from "@arcgis/core/config";
 
 import styles from "./app.module.css";
@@ -11,6 +15,8 @@ function App() {
 
   useEffect(() => {
     if (mapDiv.current) {
+
+
       esriConfig.apiKey =
       "AAPKff271af86e5e4145ae46098747021260kqF_X1V0Z8FSN9TJcZbqceQjUdHGDJ11-hNCPx8t2CVvrp_Lvbd5S64hXMVFd_CO";
 
@@ -24,6 +30,129 @@ function App() {
         zoom: 13,
         container: mapDiv.current,
       });
+
+      const serviceUrl =
+      "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+
+    view.on("click", function (evt) {
+      const params = {
+        location: evt.mapPoint,
+      };
+      locator.locationToAddress(serviceUrl, params).then(
+        function (response) {
+          const address = response.address;
+          showAddress(address, evt.mapPoint);
+        },
+        function (err) {
+          showAddress("No address found.", evt.mapPoint);
+        }
+      );
+    });
+
+    function showAddress(address, pt) {
+      view.popup.open({
+        title:
+          +Math.round(pt.longitude * 100000) / 100000 +
+          ", " +
+          Math.round(pt.latitude * 100000) / 100000,
+        content: address,
+        location: pt,
+      });
+    }
+
+    const routeUrl =
+      "https://route-api.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World";
+
+      function addGraphic(type, point) {
+        const graphic = new Graphic({
+          symbol: {
+            type: "simple-marker",
+            color: type === "origin" ? "white" : "black",
+            size: "8px",
+          },
+          geometry: point,
+        });
+        view.graphics.add(graphic);
+      }
+
+      const search = new Search({
+        view: view,
+        includeDefaultSources: false,
+        sources: [
+          {
+            url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer",
+            singleLineFieldName: "SingleLine",
+            outFields: ["Addr_type"],
+            name: "ArcGIS World Geocoding Service",
+            placeholder: "hello",
+          //   resultSymbol: {
+          //     type: "picture-marker",
+          //     url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer" + "/images/search/search-symbol-32.png",
+          //     size: 24,
+          //     width: 24,
+          //     height: 24,
+          //     xoffset: 0,
+          //     yoffset: 0
+          // }
+          },
+        ],
+      });
+
+      view.popup.watch("visible", function (visible) {
+        document
+          .querySelector(".esri-ui-manual-container")
+          .classList.remove("overlay");
+          view.ui.remove(search);
+      });
+
+      view.ui.add({
+        component: search,
+        position: "manual",
+      });
+
+      view.when(function () {
+        search.container.classList.add("widgetcenter");
+        document
+          .querySelector(".esri-ui-manual-container")
+          .classList.add("overlay");
+      });
+
+      const graphicsLayer = new GraphicsLayer();
+      map.add(graphicsLayer);
+
+      const point = {
+        type: "point",
+        longitude: -118.33987522698867,
+        latitude: 34.084543364219606,
+      };
+
+      const simpleMarkerSymbol = {
+        type: "simple-marker",
+        color: [226, 119, 40],
+        outline: {
+          color: [255, 255, 255],
+          width: 1,
+        },
+      };
+
+      const attributes = {
+        Name: "Meteora",
+        Description: "restaurant",
+      };
+
+      const popupTemplate = {
+        title: "{Name}",
+        content: "{Description}",
+      };
+
+      const pointGraphic = new Graphic({
+        geometry: point,
+        symbol: simpleMarkerSymbol,
+        attributes: attributes,
+        popupTemplate: popupTemplate,
+      });
+      graphicsLayer.add(pointGraphic);
+
     }
   }, []);
 
