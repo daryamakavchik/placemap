2494a02df60a4b1f2db9193fb9878135338d4e9e
daryamakavchik <darjamakavchik@gmail.com>
2022-12-02 18:27:18 -0800

    add closeicon and point styles

--- src/project/src/components/app/App.jsx
+++ src/project/src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 import Graphic from "@arcgis/core/Graphic";
@@ -8,28 +8,19 @@ import PopupTemplate from "@arcgis/core/PopupTemplate";
 import * as locator from "@arcgis/core/rest/locator";
 import Locate from "@arcgis/core/widgets/Locate";
 import esriConfig from "@arcgis/core/config";
-import meteora from "../../images/meteora.png";
-import daedo from "../../images/daedo.jpeg";
-import kinn from "../../images/kin.jpeg";
-import steep from "../../images/steep.jpeg";
+import { data } from "../../mock-data";
 import styles from "./app.module.css";
+import icon from '../../images/icon.png';
+import closeic from '../../images/closeic.png';
 
 function App() {
-  const [pointState, setPointState] = useState({
-    Name: "Meteora",
-    Description: "restaurant",
-    LATITUDE: 34.084543364219606,
-    LONGITUDE: -118.33987522698867,
-    image: meteora,
-  });
-
   const mapDiv = useRef(null);
   useEffect(() => {
-    if (mapDiv.current) {
-      esriConfig.apiKey =
-        "AAPKff271af86e5e4145ae46098747021260kqF_X1V0Z8FSN9TJcZbqceQjUdHGDJ11-hNCPx8t2CVvrp_Lvbd5S64hXMVFd_CO";
-      const map = new Map({ basemap: "osm-dark-gray" });
+    if (mapDiv.current) { 
+      esriConfig.apiKey = "AAPKff271af86e5e4145ae46098747021260kqF_X1V0Z8FSN9TJcZbqceQjUdHGDJ11-hNCPx8t2CVvrp_Lvbd5S64hXMVFd_CO";
+      const serviceUrl = "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
 
+      const map = new Map({ basemap: "osm-dark-gray" });
       const view = new MapView({
         map: map,
         center: [-118.243683, 34.052235],
@@ -38,6 +29,17 @@ function App() {
       });
 
       document.body.classList.add("nopointer");
+
+      function showAddress(address, pt) {
+        view.popup.open({
+          title:
+            +Math.round(pt.longitude * 100000) / 100000 +
+            ", " +
+            Math.round(pt.latitude * 100000) / 100000,
+          content: address,
+          location: pt,
+        });
+      }
 
       let locateWidget = new Locate({
         view: view,
@@ -68,9 +70,6 @@ function App() {
         );
       });
 
-      const serviceUrl =
-        "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
-
       view.on("click", function (evt) {
         const params = { location: evt.mapPoint };
         locator.locationToAddress(serviceUrl, params).then(
@@ -83,16 +82,7 @@ function App() {
           }
         );
       });
-      function showAddress(address, pt) {
-        view.popup.open({
-          title:
-            +Math.round(pt.longitude * 100000) / 100000 +
-            ", " +
-            Math.round(pt.latitude * 100000) / 100000,
-          content: address,
-          location: pt,
-        });
-      }
+
 
       const search = new Search({
         view: view,
@@ -115,53 +105,15 @@ function App() {
         search.container.classList.add("widgetcenter");
         document.querySelector(".esri-locate").classList.add("locatecenter");
         document.querySelector(".esri-locate").classList.add("zindex");
-        document
-          .querySelector(".esri-ui-manual-container")
-          .classList.add("overlay");
+        document.querySelector(".esri-ui-manual-container").classList.add("overlay");
       });
       view.popup.watch("visible", function (visible) {
-        document
-          .querySelector(".esri-ui-manual-container")
-          .classList.remove("overlay");
+        document.querySelector(".esri-ui-manual-container").classList.remove("overlay");
         view.ui.remove(search);
         document.body.classList.remove("nopointer");
       });
 
-      let data = [
-        {
-          Name: "Meteora",
-          Description: "restaurant",
-          LATITUDE: 34.08451193448912,
-          LONGITUDE: -118.33992004394125,
-          image: meteora,
-          index: 1,
-        },
-        {
-          Name: "Kinn LA",
-          Description: "restaurant",
-          LATITUDE: 34.06439220871661,
-          LONGITUDE: -118.30807685851646,
-          image: kinn,
-          index: 2,
-        },
-        {
-          Name: "STEEP LA",
-          Description: "restaurant",
-          LATITUDE: 34.06688077298084,
-          LONGITUDE: -118.23572158813074,
-          image: steep,
-          index: 3,
-        },
-        {
-          Name: "Daedo Sikdang",
-          Description: "restaurant",
-          LATITUDE: 34.064463311566975,
-          LONGITUDE: -118.31078052520347,
-          image: daedo,
-          index: 4,
-        },
-      ];
-
+      
       let graphics = [];
       for (let i = 0; i < data.length; i++) {
         let graphic = new Graphic({
@@ -179,31 +131,23 @@ function App() {
         view.hitTest(event).then(function (response) {
           if (response.results.length) {
             var graphic = response.results.filter(function (result) {
-              return result.graphic.layer === layer;
+              return result.graphic && result.graphic.layer && result.graphic.layer === layer;
             })[0].graphic;
             view.popup.open({
-              location: event.mapPoint,
+              location: graphic.geometry.centroid,
               features: [graphic],
             });
+            view.popup.highlightEnabled = false;
           }
         });
       });
 
       let popupTemplate = new PopupTemplate({
-        title: () =>
-          `${
-            data.filter(
-              (el) => view.popup.features[0].attributes.OBJECTID === el.OBJECTID
-            )[0].Name
-          }`,
+        title: () =>`${ data.filter((el) => view.popup.features[0].attributes.OBJECTID === el.OBJECTID)[0].Name }`,
         content: () => {
           let div = document.createElement("div");
           div.className = styles.class;
-          div.style.backgroundImage = `url(${
-            data.filter(
-              (el) => view.popup.features[0].attributes.OBJECTID === el.OBJECTID
-            )[0].image
-          })`;
+          div.style.backgroundImage = `url(${data.filter((el) => view.popup.features[0].attributes.OBJECTID === el.OBJECTID)[0].image})`;
           return div;
         },
       });
@@ -217,21 +161,15 @@ function App() {
             name: "OBJECTID",
             type: "oid",
           },
-          {
-            name: "image",
-            type: "string",
-          },
         ],
         renderer: {
           type: "simple",
+          declaredClass: 'zindex',
           symbol: {
-            type: "text",
-            color: "#7A003C",
-            text: "\ue661",
-            font: {
-              size: 20,
-              family: "CalciteWebCoreIcons",
-            },
+            type: "picture-marker",
+          url: icon,
+          width: "70px",
+          height: "70px",
           },
         },
       });
@@ -240,7 +178,7 @@ function App() {
     }
   }, []);
 
-  return <div className={styles.webmap} ref={mapDiv}></div>;
+  return <div className={styles.webmap} ref={mapDiv}><button className={styles.closebutton}><img className={styles.closeicon} src={closeic} /></button></div>;
 }
 
 export default App;
---
+++ src/project/src/mock-data.jsx
@@ -0,0 +1,39 @@
+import meteora from "../src/images/meteora.png";
+import daedo from "../src/images/daedo.jpeg";
+import kinn from "../src/images/kin.jpeg";
+import steep from "../src/images/steep.jpeg";
+
+export const data = [
+    {
+      Name: "Meteora",
+      Description: "restaurant",
+      LATITUDE: 34.08451193448912,
+      LONGITUDE: -118.33992004394125,
+      image: meteora,
+      index: 1,
+    },
+    {
+      Name: "Kinn LA",
+      Description: "restaurant",
+      LATITUDE: 34.06439220871661,
+      LONGITUDE: -118.30807685851646,
+      image: kinn,
+      index: 2,
+    },
+    {
+      Name: "STEEP LA",
+      Description: "restaurant",
+      LATITUDE: 34.06688077298084,
+      LONGITUDE: -118.23572158813074,
+      image: steep,
+      index: 3,
+    },
+    {
+      Name: "Daedo Sikdang",
+      Description: "restaurant",
+      LATITUDE: 34.064463311566975,
+      LONGITUDE: -118.31078052520347,
+      image: daedo,
+      index: 4,
+    },
+  ];
